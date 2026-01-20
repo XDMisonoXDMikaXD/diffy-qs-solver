4e863779ca2bf9e53baf893c0e03e69ac2679dbe
XDMisonoXDMikaXD <phucql1@uci.edu>
2026-01-20 02:09:04 -0800

    adding matplotlib and numpy

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ import matplotlib.pyplot as plt
 
 #A Primer in Numerics: Ordinary Differential Equations
 
-#choose step size 
-dt = 0.1 
-#choose interval
-T = 10
+def main():
+    #choose step size 
+    dt = 0.1 
+    #choose interval
+    T = 10
 
-#set up parameters
-w = 2*math.pi #natural frequency
-z = 0.25      #damping coefficient
-A = np.array([[0,1],
-             [-w**2, -2*z*w]]) #spring-mass-damp system
+    #set up parameters
+    w = 2*math.pi #natural frequency
+    z = 0.25      #damping coefficient
+    A = np.array([[0,1],
+                 [-w**2, -2*z*w]]) #spring-mass-damp system
 
-x0 = np.array([2,1]) #initial conditions
+    x0 = np.array([2,1]) #initial conditions
 
-print(x0)
+    print(x0)
 
 
+def delta_x(x1, x2, y1, y2):
+    return (y2 - y1) / (x2 - x1)
 
+
+class coordinate_system:
+    x = [startx, endx, stepx]
+    y = np.zeros(len(x))
+    
+
+if __name__ == "__main__":
+    main()
